1b1741a56ed55cde8e1d598c29609fbe8e78c190
marielm5 <marielm5@uci.edu>
2023-02-22 17:57:49 -0800

    edit d_prime function

--- hw_3/assignment2_main.m
+++ hw_3/assignment2_main.m
@@ -23,9 +23,8 @@ classdef SignalDetection
             FA = obj.falseAlarms/(obj.falseAlarms + obj.correctRejections);
    
         function d_prime = d_prime(obj)
-            hitRate = obj.hits / (obj.hits + obj.misses);
-            falseAlarmRate = obj.falseAlarms / (obj.falseAlarms + obj.correctRejections);
-            d_prime = (norminv(hitRate) - norminv(falseAlarmRate));
+           % calculate d-prime
+            d_prime =  norminv(hit_rate(obj)) - norminv(obj.FA());
         end
 
         function criterion = criterion(obj)
